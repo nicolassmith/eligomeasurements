68e0d21f62a725ac6e0b476f363bf048f499f67a
nicolassmith <nicolas.d.smith@gmail.com>
2010-09-01 17:26:18 -0400

    more OMC length noise stuff, including data from power to power transfer function measurements.

---
+++ OMClengthnoise/2010-08-23/broadbandDARMinjection.m
@@ -0,0 +1,38 @@
+% look at broadband OMC length injection to DARM
+
+% load spectra
+
+[PZTOUTnoinj,DARMnoinj] = DTTloadspec('07-noexcitation-pztlscout-darmerr.txt');
+
+[PZTOUTinj,DARMinj] = DTTloadspec('08-biggerexcitation-pztlscout-darmerr.txt');
+
+% load DARM calibration
+
+load('../../DCnoisecouplings/DARMcalib.mat')
+
+% calibrate DARM to meters
+
+DARMnoinj = abs(calTF(DARMnoinj,DARMcalib));
+DARMinj = abs(calTF(DARMinj,DARMcalib));
+
+% make plot
+
+figure(43)
+
+subplot(2,1,1)
+SRSspec(PZTOUTnoinj,PZTOUTinj)
+
+subplot(2,1,2)
+SRSspec(DARMnoinj,DARMinj)
+
+% make subtractions
+
+DARMex = [DARMnoinj(:,1),realsqrt(DARMinj(:,2).^2 - DARMnoinj(:,2).^2)]; % USE QUADSUBTRACT HERE
+PZTOUTex = [PZTOUTnoinj(:,1),realsqrt(PZTOUTinj(:,2).^2 - PZTOUTnoinj(:,2).^2)];
+
+figure(44)
+subplot(2,1,1)
+SRSspec(PZTOUTex)
+
+subplot(2,1,2)
+SRSspec(DARMex)
