e0e028ceb98f03a70ed9a4468a5cb8df24382fe7
Tobin Fricke <tobin@pas.rochester.edu>
2011-06-07 18:55:46 -0500

    MOD_2_EXC delay appears to be about 2 ms

--- DCnoisecouplings/oan/L1/2010-07-06/AM_calib_check.m
+++ DCnoisecouplings/oan/L1/2010-07-06/AM_calib_check.m
@@ -1,4 +1,9 @@
-%%
+%% Marconi Oscillator AM modulation-depth calibration check
+%
+% To check the calibration of the modulation drive, I configured a
+% single-bounce off an ITM and locked the OMC to the sideband.  This
+% directly measures AM of the RF sideband, which can be easily related to
+% the AM on the oscillator.
 
 %%
 set(0 ,'DefaultLineLineWidth', 2)
@@ -8,9 +13,10 @@ filenames = {'OSCAM_SBSB_amp=0.14142', 'OSCAM_SBSB_amp=1.4142'};
 cm = jet();
 hold off
 
-exc_calib = 0.05 / sqrt(2); % RAM / volts
+exc_calib = 0.05 / sqrt(2);   % RAM / volts
 omc_calib = 1/0.405;          % RIN / count
 
+% Load the data files
 for ii=1:length(filenames)
     filename = filenames{ii};    
     data(ii).filename = filename;
@@ -21,19 +27,17 @@ for ii=1:length(filenames)
     data(ii).coh = temp(:,2:end);
 end
 
-Txy = data(1).Txy(:,5) * omc_calib / exc_calib;
-coh = data(1).coh(:,5);
-Navg = 10;
-err = sqrt((1 - coh)./(2 * Navg * coh));
+for ii=1:2,
+    Txy{ii}.Txy = data(ii).Txy(:,5) * omc_calib / exc_calib;
+    Txy{ii}.f = data(ii).f;
+end
 
-plot(data(1).f, db(Txy),'o-', 'color',[0.5 1 0.5 ])
+%% Amplitude
+
+plot(Txy{1}.f, db(Txy{1}.Txy), 'go-', 'markersize', 10);
 hold all
-
-Txy = data(2).Txy(:,5) * omc_calib / exc_calib;
-coh = data(2).coh(:,5);
-Navg = 10;
-err = sqrt((1 - coh)./(2 * Navg * coh));
-plot(data(1).f, db(Txy),'o-', 'color', [0 0.5 0])
+plot(Txy{2}.f, db(Txy{2}.Txy), 'o-', 'color', [0 0.5 0]);
+hold off
 
 set(gca, 'xscale', 'log');
 xlabel('frequency');
@@ -43,7 +47,49 @@ grid on;
 Gamma = 0.32; 
 expected_level = Gamma * (besselj(0, Gamma) - besselj(2, Gamma))/besselj(1, Gamma);
 
-line(get(gca,'xlim'), db(expected_level)  * [ 1 1], 'color', [0.1 0.88 0.1]);
-legend('exc amp 0.1 volts RMS', 'exc amp 1.0 volts RMS', '\Gamma \cdot ( \partial/\partial\Gamma J_1(\Gamma)) / J_1(\Gamma) with \Gamma = 0.32');  
+line(get(gca,'xlim'), db(expected_level)  * [ 1 1], 'color', [0 0 1]);
+legend('exc amp 0.1 volts RMS', 'exc amp 1.0 volts RMS', ...
+       '\Gamma \cdot ( \partial/\partial\Gamma J_1(\Gamma)) / J_1(\Gamma) with \Gamma = 0.32', ...
+       'Location', 'SouthWest');  
 xlim([20 8000])
 hold off
+
+%% Phase (unwrapped)
+% a delay generates a linear phase:
+%
+% phase [radians] = tau [s] * f [cycles/second] * (2pi) [radians/cycle]
+
+plot(Txy{2}.f, unwrap(angle(Txy{2}.Txy)) * 180/pi, 'o-', 'color', [0 0.5 0]);
+
+xlabel('frequency');
+ylabel('phase[degrees]');
+grid on;
+title('OH HO HO, LOOKS LIKE THERE''S A DELAY');
+
+%% Delay is about 2 ms
+
+% The delay appears to be very close to 2 ms:
+tau_est0 = 2e-3; % [s]
+
+% Here's one way to get the delay:
+f_probe = 200;
+tau_est1 = interp1(Txy{2}.f, unwrap(angle(Txy{2}.Txy)), f_probe)/(2*pi)/f_probe
+
+% Here is another way, doubltessly overkill:
+ii = Txy{2}.f < 2500;
+fitfcn = @(tau, f) tau * f * 2*pi;
+plot(Txy{2}.f, unwrap(angle(Txy{2}.Txy)), 'o', ...
+     Txy{2}.f(ii), fitfcn(tau, Txy{2}.f(ii)), '-');
+tau_est2 = lsqcurvefit(fitfcn, tau_est1, ...
+    Txy{2}.f(ii), unwrap(angle(Txy{2}.Txy(ii))))
+
+tau = tau_est2;
+
+delay_fix = exp(-1i * 2*pi * tau * Txy{2}.f );
+semilogx(Txy{2}.f, angle(Txy{2}.Txy .* delay_fix) * 180/pi, 'o-', 'color', [0 0.5 0]);
+
+%set(gca, 'xscale', 'log');
+xlabel('frequency');
+ylabel('phase');
+grid on;
+title('Delay mostly removed');
